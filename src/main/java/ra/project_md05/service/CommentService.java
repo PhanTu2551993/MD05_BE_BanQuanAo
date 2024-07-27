0d43375f7de633b1aa4435dac146bc4c132d3914
PhanTu2551993 <phanvantu25051993@gmail.com>
2024-07-27 17:33:59 +0700

    tuday7_2

--- src/main/java/ra/project_md05/service/CommentService.java
+++ src/main/java/ra/project_md05/service/CommentService.java
@@ -1,8 +1,13 @@
 package ra.project_md05.service;
 
+import ra.project_md05.model.dto.request.CommentsRequest;
 import ra.project_md05.model.entity.Comment;
 
+import java.util.List;
+
 public interface CommentService {
+    Comment addComment(CommentsRequest commentsRequest);
+    List<Comment> getCommentsByProduct(Long productId);
     void deleteComment(Long id);
 
     Comment updateCommentStatus(Long id, Boolean status);
